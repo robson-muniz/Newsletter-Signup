4f6c035ab9c70b4db467d25dc9246dda8d1025fd
robson-muniz <robson_muniz@yahoo.com>
2019-09-17 20:47:52 -0300

    Post Data to MailChimps Servers.

--- app.js
+++ app.js
@@ -17,10 +17,47 @@ app.post("/", function(req, res){
     var lastName = req.body.lName;
     var email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    var data = {
+        members:[
+            {email_address: email,
+            status:"subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            }
+        }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url: 'https://us4.api.mailchimp.com/3.0/lists/7e10e5ddc1',
+        method: "POST",
+        headers:{
+            "authorization": "robson1 8fd96535136d57b19b5fc58429529927-us4"
+        },
+        body: jsonData,
+
+    };
+
+    request(options, function(error, response, body){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(response.statusCode);
+        }
+
+    });
 
 })
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000.");
 });
+
+//API Key
+//8fd96535136d57b19b5fc58429529927-us4
+
+//ID for Audience Robson Muniz
+//7e10e5ddc1
